7acd085a2f2144f622560112d6b93fce1eb57607
SandroPaiva <sandroneripaiva@gmail.com>
2026-07-12 10:35:38 -0300

    adição das tabelas categorias, fabricantes e plataformas ao banco

--- setup.sql
+++ setup.sql
@@ -9,3 +9,25 @@ GRANT ALL PRIVILEGES ON lojajogos.* TO 'loja_user'@'localhost';
 
 -- 4. Aplica as mudanças de privilégios imediatamente
 FLUSH PRIVILEGES;
+
+use lojajogos;
+
+CREATE TABLE categorias (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL UNIQUE, -- Esse campo impede o cadastro de mais de uma caterogira com o mesmo nome.
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE fabricantes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL UNIQUE, -- Esse campo impede o cadastro de mais de uma caterogira com o mesmo nome.
+    site_url VARCHAR(250) NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE plataformas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL UNIQUE, -- Esse campo impede o cadastro de mais de uma caterogira com o mesmo nome.
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
